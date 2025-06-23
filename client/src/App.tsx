59a16773afb040b9eca586e6f88c1517cdb4de8b
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-06-23 12:40:37 +0300

    Yet another approach

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,13 @@ import "./App.css";
 const viewSize = 1000;
 
 function App() {
-  const [zoom, setZoom] = useState(1);
+  const [zoom, setZoom] = useState(0);
 
   function handleZoom(increment: number) {
     setZoom((zoom) => {
       const newZoom = zoom + increment;
 
-      return newZoom <= 0 ? 1 : newZoom > 10000 ? 10000 : newZoom;
+      return newZoom < 0 ? 10 : newZoom > 10 ? 0 : newZoom;
     });
   }
 
@@ -19,79 +19,92 @@ function App() {
 
     handleZoom(increment);
   }
+  console.log(zoom);
+  // const relativeSize = viewSize * Math.pow(1.01, zoom);
 
-  const relativeSize = viewSize * Math.pow(1.01, zoom);
+  const viewBoxSize = viewSize - zoom * 100;
 
-  console.log(relativeSize);
+  console.log(viewBoxSize);
+
+  const items = [
+    [{
+      name: "orca",
+      x: 10,
+      y: viewSize - 500,
+      width: 800,
+      height: 100,
+    }, {
+      name: "human",
+      x: 100,
+      y: viewSize - 200,
+      width: 50,
+      height: 200,
+    }],
+    [{
+      name: "cat",
+      x: 100,
+      y: 600,
+      width: 400,
+      height: 400,
+    }],
+    [{
+      name: "mouse",
+      x: 100,
+      y: 400,
+      width: 400,
+      height: 200,
+    }],
+  ];
 
   return (
-    <div>
-      <svg
+    <div style={{ display: "flex" }}>
+      <div
         onWheel={handleWheelEvent}
-        width="1000"
-        height="1000"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ border: "1px solid black" }}
       >
-        <text x="20" y="20">Zoom {Math.floor(relativeSize / viewSize)} x</text>
-        <Rectangle w={500} zoom={zoom} name="Grain of salt (0.5 mm)" />
-        <Rectangle w={100} zoom={zoom} name="Large cell (100 µm)" />
-        <Rectangle w={10} zoom={zoom} name="Small cell (10 µm)" />
-        <Rectangle
-          w={1}
-          zoom={zoom}
-          name="Bacteria / Mitochondria / Chromosome (1 µm)"
-        />
-        <Rectangle
-          w={0.1}
-          zoom={zoom}
-          name="Flu virus / Largest molecule (100 nm)"
-        />
-        <Rectangle w={0.01} zoom={zoom} name="Protein (10 nm)" />
-        <Rectangle w={0.001} zoom={zoom} name="Lipid (1 nm)" />
-        <Rectangle w={1e-4} zoom={zoom} name="Atom (0.1 nm)" />
-      </svg>
-      {
-        /* <button
-        style={{ width: "100px", height: "100px", fontSize: "50px" }}
-        type="button"
-        onClick={() => handleZoom(1)}
-      >
-        +
-      </button>
-      <button
-        style={{ width: "100px", height: "100px", fontSize: "50px" }}
-        type="button"
-        onClick={() => handleZoom(-1)}
-      >
-        -
-      </button> */
-      }
+        <svg
+          width={viewSize}
+          height={viewSize}
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ border: "1px solid black" }}
+          viewBox={`0 ${viewSize - viewBoxSize} ${viewBoxSize} ${viewBoxSize}`}
+        >
+          {items.flatMap((ims, i) => {
+            const multiplier = Math.pow(10, i);
+            return (
+              ims.map((item) => {
+                return (
+                  <rect
+                    key={item.name}
+                    x={item.x / multiplier}
+                    y={viewSize - (viewSize - item.y) / multiplier}
+                    width={item.width / multiplier}
+                    height={item.height / multiplier}
+                    fill="green"
+                  />
+                );
+              })
+            );
+          })}
+        </svg>
+      </div>
+      <div>
+        <button
+          style={{ width: "100px", height: "50px", fontSize: "20px" }}
+          type="button"
+          onClick={() => handleZoom(1)}
+        >
+          +
+        </button>
+        <button
+          style={{ width: "100px", height: "50px", fontSize: "20px" }}
+          type="button"
+          onClick={() => handleZoom(-1)}
+        >
+          -
+        </button>
+      </div>
     </div>
-  );
-}
-
-function Rectangle({ w = 1, zoom = 1, name = "foo" }) {
-  const size = w * Math.pow(1.01, zoom);
-
-  return (
-    <>
-      <rect
-        x={size / 2}
-        y={1000 - size}
-        width={size}
-        height={size}
-        fill="green"
-      />
-      <text
-        x={size / 2}
-        y={1000 - size - size / 60}
-        style={{ fontSize: `${size / 20}px` }}
-      >
-        {name}
-      </text>
-    </>
   );
 }
 
